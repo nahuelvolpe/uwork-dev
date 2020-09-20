169b1d396c5d136fd792ab71948d694a7a7bbc95
Matias Gimenez <meganiunmati@hotmail.com>
2020-09-20 15:53:59 -0300

    wip layout using context

---
+++ src/uWork/components/Layout/Layout.js
@@ -0,0 +1,59 @@
+import React, { useContext, Fragment } from 'react'
+import { AuthContext } from '../../context/auth'
+import { AppBar, Toolbar, IconButton, Typography, makeStyles, Container } from '@material-ui/core'
+import { AccountCircle } from '@material-ui/icons'
+import MenuIcon from '@material-ui/icons/Menu'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
+
+const Layout = (props) => {
+  const { isLoggedIn } = useContext(AuthContext)
+  const classes = useStyles();
+  const { children } = props;
+  return (
+    <div>
+      {isLoggedIn &&
+        (
+          <Fragment>
+            <AppBar position="static">
+              <Toolbar>
+                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" className={classes.title}>
+                  Photos
+                </Typography>
+                <div>
+                  <IconButton
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    color="inherit"
+                  >
+                    <AccountCircle />
+                  </IconButton>
+                </div>
+              </Toolbar>
+            </AppBar>
+          </Fragment>)}
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Container fixed>
+          {children}
+        </Container>
+      </main>
+    </div>
+  )
+}
+
+export default Layout
--- src/uWork/uWork.jsx
+++ src/uWork/uWork.jsx
@@ -4,20 +4,23 @@ import { AuthContextProvider } from './context/auth'
 import Login from './components/Login/Login'
 import AfterSignup from './components/AfterSignup'
 import Register from "./components/Register/Register"
+import Layout from './components/Layout/Layout'
 
 const uWorkApp = () => {
   return (
     <div>
       <BrowserRouter>
         <AuthContextProvider>
-          <Switch>
-            <Route exact path="/" render={props => <Login {...props} />} />
-            <Route exact path="/login" render={props => <Login {...props} />} />
-            <Route exact path="/aftersignup" render={props => <AfterSignup {...props} />} />
-            <Route exact path="/register" render={props => <Register {...props} />} />
-            {/* <AuthenticatedRoute exact path="/news" render={props => <NewsDashboard />} /> */}
-            {/* <Route render={props => <ErrorComponent />} /> */}
-          </Switch>
+          <Layout>
+            <Switch>
+              <Route exact path="/" render={props => <Login {...props} />} />
+              <Route exact path="/login" render={props => <Login {...props} />} />
+              <Route exact path="/aftersignup" render={props => <AfterSignup {...props} />} />
+              <Route exact path="/register" render={props => <Register {...props} />} />
+              {/* <AuthenticatedRoute exact path="/news" render={props => <NewsDashboard />} /> */}
+              {/* <Route render={props => <ErrorComponent />} /> */}
+            </Switch>
+          </Layout>
         </AuthContextProvider>
       </BrowserRouter>
     </div>
