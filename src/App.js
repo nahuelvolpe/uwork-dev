6a668433569ec66c2bc86cf29a6bde8853c22ab8
Matias Gimenez <meganiunmati@hotmail.com>
2020-09-20 06:12:54 -0300

    Theme provider added

--- src/App.js
+++ src/App.js
@@ -1,51 +1,21 @@
 import React from 'react';
 import UWorkApp from './uWork/uWork';
 import './App.css';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: '#14A7D6' },
+  }
+})
 
 function App() {
-
-  /* const handleLogout = () => {
-    fire.auth().signOut();
-   }
-
-  const authListener = () => {
-    fire.auth().onAuthStateChanged(user => {
-      if (user) {
-        clearInputs();
-        setUser(user);
-        console.log(user);
-      } else {
-        setUser('');
-      }
-    })
-  }
-
-  useEffect(() => {
-    authListener();
-  }, []) */
-
   return (
-    <div className="App">
-      <UWorkApp />
-      {/* user ? (
-        <AfterSignup
-          handleLogout={handleLogout}
-       /> 
-      ) : (
-        <Login 
-          email={email}
-          setEmail={setEmail}
-          password={password}
-          setPassword={setPassword}  
-          handleLogin={handleLogin}
-          handleSignup={handleSignup}
-          hasAccount={hasAccount}
-          setHasAccount={setHasAccount}
-          emailError={emailError}
-          passwordError={passwordError}
-        />
-      ) */}
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <UWorkApp />
+      </div>
+    </ThemeProvider>
   );
 }
 
