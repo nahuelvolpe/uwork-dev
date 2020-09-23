e3a26a9ae9832e7f930dd975e7bccd334acf0d64
Nahuel <vnahuelnicolas@gmail.com>
2020-09-23 17:41:11 -0300

    loadingscreenwork

--- src/uWork/components/LoadingScreen.js
+++ src/uWork/components/LoadingScreen.js
@@ -1,14 +1,26 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import { Redirect } from 'react-router-dom';
 import { AuthContext } from '../context/auth';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
 const LoadingScreen = (props) => {
 
-    
+    /* const [routeFrom, setRouteFrom] = useState('noroute')
 
-    const routeFrom = props.history.location.state.routeFrom;
-    console.log(routeFrom)
+    if(props.history.location.state){
+        setRouteFrom(props.history.location.state.routeFrom);
+        console.log(routeFrom);
+    } 
+    */
+
+    var routeFrom = 'noroute'
+
+    if(props.history.location.state){
+        routeFrom = props.history.location.state.routeFrom;
+    } 
+    
+    
+    console.log(routeFrom) 
     const { isLoggedIn } = useContext(AuthContext);
     console.log(isLoggedIn)
 
@@ -22,8 +34,11 @@ const LoadingScreen = (props) => {
     }else if (isLoggedIn && routeFrom === 'register') {
         return <Redirect to="/editprofile" />;
     }else if (isLoggedIn && routeFrom === 'login') {
+        console.log('redirect to dashboard from login')
         return <Redirect to="/dashboard" />;
-    }
+    } else if (isLoggedIn && routeFrom === 'noroute') {
+        return <Redirect to="/dashboard" />;
+    } 
 }
  
 export default LoadingScreen;
