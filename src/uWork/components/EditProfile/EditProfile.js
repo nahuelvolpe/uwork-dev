869c9861727fd7ece3f2fa2929f2fc0db9e98304
Nahuel <vnahuelnicolas@gmail.com>
2020-09-30 19:47:21 -0300

    upload image cosmetica xD

--- src/uWork/components/EditProfile/EditProfile.js
+++ src/uWork/components/EditProfile/EditProfile.js
@@ -1,27 +1,52 @@
 import React, { useState } from 'react';
-import { Button, Grid,  makeStyles } from "@material-ui/core";
+import { Button, Grid, LinearProgress, Typography, Box, Avatar,  makeStyles } from "@material-ui/core";
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import { Formik, Form } from "formik";
 import FormikField from "../FormikField/FormikField";
-import { db } from '../../services/firebase';
+import { auth, db, storage } from '../../services/firebase';
 import { AuthContext } from '../../context/auth';
+import { CloudUploadRounded } from '@material-ui/icons';
 
 
 
 const useStyles = makeStyles((theme) => ({
+    editProfileContent: {
+      display: "flex",
+      alignItems: "center",
+    },
     textField: {
       marginBottom: theme.spacing(2),
       width: "100%",
     },
-    boton: {
+    botonGuardar: {
+      paddingTop: theme.spacing(1),
+      paddingBottom: theme.spacing(1),
       marginTop: theme.spacing(2),
       marginBottom: theme.spacing(2),
       width: "50%",
+      color: 'white'
+    },
+    botonImagen: {     
+      marginTop: theme.spacing(1),
       color: 'white'
     },
     title: {
       marginBottom: theme.spacing(3),
       color: 'black'
     },
+    progress: {
+      marginTop: theme.spacing(1),
+      marginBottom: theme.spacing(1),
+      width: "50%",
+      marginBottom: theme.spacing(2)
+    },
+    avatar: {
+      width: theme.spacing(9),
+      height: theme.spacing(9),
+    },
+    input: {
+      display: "none"
+    }
   }));
 
 const EditProfile = (props) => {
@@ -29,13 +54,34 @@ const EditProfile = (props) => {
     const [apellido,] = useState('')
     const [mail,] = useState('')
     const [update, setUpdate] = useState(false)
+    const [progress, setProgress] = useState(0);
+    const [progressFull, setProgressFull] = useState(false);
+    const [imgUserUrl, setImgUserUrl] = useState('');
 
 
       const classes = useStyles();
 
-      const docUserID = props.location.state.docUserID;
+      const docUserID = props.location.state.docUserID
+
+      const currentUserID = auth.currentUser.uid;
 
       console.log(docUserID);
+      console.log(currentUserID);
+
+      function LinearProgressWithLabel(props) {
+        return (
+          <Box display="flex" alignItems="center">
+            <Box width="100%" mr={1}>
+              <LinearProgress variant="determinate" {...props} />
+            </Box>
+            <Box minWidth={35}>
+              <Typography variant="body2" color="textSecondary">{`${Math.round(
+                props.value,
+              )}%`}</Typography>
+            </Box>
+          </Box>
+        );
+      }
 
       const onSubmit = (values, { setFieldError }) => {
         db.collection('users').doc(docUserID).update(
@@ -44,9 +90,46 @@ const EditProfile = (props) => {
             lastName: values.apellido
           }
         ).then( () => {
-          setUpdate(true);
+
+            setUpdate(true);
+         
         })
       }
+
+     const chooseFile = (e) => {
+       var file = e.target.files[0];
+
+       var storageRef = storage.ref('users/' + currentUserID + '/profile.jpg');
+
+       /* storageRef.put(file).then(() => {
+         setUploader(true);
+       }).catch( (e) => {
+         console.log(e);
+       }) */
+
+       var task = storageRef.put(file);
+
+       task.on('state_changed', 
+        function progress(snapshot){
+          var percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setProgress(percentage)
+        },
+        function error(err) {
+
+        },
+        function complete() {
+          getUrlImage();
+          setProgressFull(true);
+        }
+       );
+     }
+
+     const getUrlImage = () => {
+      var storageRef = storage.ref('users/' + currentUserID + '/profile.jpg');
+      storageRef.getDownloadURL().then( imgUrl =>{
+        setImgUserUrl(imgUrl);
+      })
+     }
 
       return (
         <div>
@@ -60,12 +143,38 @@ const EditProfile = (props) => {
               justify="center"
             >
               <div><h1 className={classes.title}>EDITAR PERFIL</h1></div>
+                {progressFull ? (
+                  <Avatar className={classes.avatar} src={imgUserUrl} />
+                  ): (
+                  <Avatar className={classes.avatar} >U</Avatar>
+                )}
+                {/* <LinearProgressWithLabel className={classes.progress} value={progress} /> */}
+                      <input
+                        className={classes.input}
+                        id="contained-button-file"
+                        multiple
+                        type="file"
+                        onChange={chooseFile}
+                      />
+                      <label htmlFor="contained-button-file">
+                        <Button className={classes.botonImagen} 
+                          variant="contained" 
+                          color="primary" 
+                          component="span"
+                          startIcon={<CloudUploadIcon />}>
+                          Subir imagen
+                        </Button>
+                      </label>
                 <Formik
                   initialValues={{ nombre, apellido, mail }}
                   onSubmit={onSubmit}>
                   {({ dirty, isValid, errors, touched }) => (
                       <Form>
-                      <input type="file"/> 
+                      
+                      
+
+                      
+
                       <FormikField className={classes.textField} label="Nombre" id="user-name" name="nombre" type="text"
                         fullWidth />
 
@@ -75,7 +184,7 @@ const EditProfile = (props) => {
                       {/* <FormikField className={classes.textField} label="Correo Electrónico" id="user-mail" name="mail" 
                       type="text" fullWidth /> */}
 
-                      {<Button className={classes.boton}
+                      {<Button className={classes.botonGuardar}
                         variant="contained"
                         color="primary"
                         type="submit">
@@ -89,7 +198,7 @@ const EditProfile = (props) => {
                   <h2>Datos guardados!</h2>
                 ):(
                   null
-                )}
+                )}                
             </Grid>
           </Grid>
         </div>
