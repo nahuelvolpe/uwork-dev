6aa287dce982579d225968c41f8b3550f107d828
Matias Gimenez <meganiunmati@hotmail.com>
2020-10-09 03:23:21 -0300

    subject popup functional

--- src/uWork/components/Dashboard/AddSubject.js
+++ src/uWork/components/Dashboard/AddSubject.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
@@ -12,6 +12,8 @@ import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import Select from '@material-ui/core/Select';
 import Switch from '@material-ui/core/Switch';
+import careers from '../../services/subjects/careers.json'
+import subjects from '../../services/subjects/subjects.json'
 
 const useStyles = makeStyles((theme) => ({
   form: {
@@ -29,28 +31,36 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function AddSubject({carrera, subject, open, setOpen, setCarrera, setSubject}) {
+export default function AddSubject(props) {
+  const { open, setOpen, acceptHandler } = props
   const classes = useStyles();
-  
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
+
+  const [subject, setSubject] = useState({})
+  const [career, setCareer] = useState({})
+  const [subjectOptions, setOptions] = useState([])
 
   const handleClose = () => {
     setOpen(false);
   };
 
   const handleCarrera = (e) => {
-    setCarrera(e.target.value)
+    setCareer(e.target.value)
+    let filteredOptions = subjects.filter(s => s.career === e.target.value.id)
+    setOptions(filteredOptions)
   }
   const handleSubject = (e) => {
     setSubject(e.target.value)
   }
 
+  const onAccept = () => {
+    acceptHandler({ career: career.description, subject: subject.description })
+    setOpen(false)
+  }
+
 
   return (
     <React.Fragment>
-      
+
       <Dialog
         open={open}
         onClose={handleClose}
@@ -66,36 +76,40 @@ export default function AddSubject({carrera, subject, open, setOpen, setCarrera,
               <InputLabel htmlFor="carrera">Carrera</InputLabel>
               <Select
                 autoFocus
-                value={carrera}
+                value={career}
                 onChange={handleCarrera}
-                inputProps={{
-                  name: 'carrera',
-                  id: 'carrera',
-                }}
               >
-                <MenuItem value="informatica">Licenciatura en Informática</MenuItem>
+                {
+                  careers.map(
+                    (career) => <MenuItem key={career.id} value={career}>{career.description}</MenuItem>
+                  )
+                }
               </Select>
-            </FormControl>   
+            </FormControl>
             <FormControl className={classes.formControl}>
               <InputLabel htmlFor="subject">Materia</InputLabel>
               <Select
                 autoFocus
                 value={subject}
                 onChange={handleSubject}
-                inputProps={{
-                  name: 'subject',
-                  id: 'subject',
-                }}
               >
-                <MenuItem value="algoritmos">Algoritmos y Estructuras de Datos</MenuItem>
+                {
+                  subjectOptions.map(
+                    subject => <MenuItem key={subject.id} value={subject}>{subject.description}</MenuItem>
+                  )
+                }
+                {/* <MenuItem value="algoritmos">Algoritmos y Estructuras de Datos</MenuItem>
                 <MenuItem value="poo">Programacion con Objetos</MenuItem>
                 <MenuItem value="base-datos">Base de datos</MenuItem>
-                <MenuItem value="tecnologia-aplicada">Tecnología Aplicada</MenuItem>
+                <MenuItem value="tecnologia-aplicada">Tecnología Aplicada</MenuItem> */}
               </Select>
-            </FormControl>         
+            </FormControl>
           </form>
         </DialogContent>
         <DialogActions>
+          <Button onClick={onAccept} color="primary">
+            Aceptar
+          </Button>
           <Button onClick={handleClose} color="primary">
             Cerrar
           </Button>
--- src/uWork/routers/AppRouter.js
+++ src/uWork/routers/AppRouter.js
@@ -7,9 +7,8 @@ import Login from '../components/Login/Login';
 import Register from '../components/Register/Register';
 import Dashboard from '../components/Dashboard/Dashboard';
 import EditProfile from '../components/EditProfile/EditProfile';
-import CircularProgress from '@material-ui/core/CircularProgress';
 import Layout from '../components/Layout/Layout';
-
+import LoadingPage from '../components/LoadingPage/LoadingPage';
 
 const AppRouter = () => {
 
@@ -27,7 +26,7 @@ const AppRouter = () => {
                         <PrivateRoute exact path="/dashboard" component={Dashboard} />
                     </Layout>
                 </Switch>
-                : <CircularProgress />}
+                : <LoadingPage />}
         </div>
     )
 }
