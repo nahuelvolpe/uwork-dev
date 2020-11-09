d53dd80758faadd5f396f1d0d6def4a6f280223c
Nahuel <vnahuelnicolas@gmail.com>
2020-11-09 16:47:17 -0300

    tabs para las listas de tareas

--- src/uWork/components/Subject/Subject.js
+++ src/uWork/components/Subject/Subject.js
@@ -1,7 +1,9 @@
 import React, {useContext, useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom'
-import { Grid, IconButton, makeStyles, Button, Paper } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import { Grid, IconButton, makeStyles, Button, Paper, AppBar, Tabs, Tab, Typography, Box } from '@material-ui/core';
 import PersonAddIcon from '@material-ui/icons/PersonAdd';
+import PostAddIcon from '@material-ui/icons/PostAdd';
 import Invite from './Invite';
 import { SubjectContext } from '../../context/subject';
 import * as MateriasService from '../../services/MateriasService'
@@ -16,32 +18,22 @@ const useStyles = makeStyles((theme) => ({
         bottom: 0,
         right: 0,
         marginBottom: '12px',
-        marginRight: '12px',
+        marginRight: '8px',
         color: 'white',
-        backgroundColor: theme.palette.primary.main
-    },
-    floatingButtonCollabs: {
-        position: 'fixed',
-        bottom: 0,
-        left: 0,
-        marginBottom: '12px',
-        marginLeft: '12px',
-        color: 'white',
-        backgroundColor: theme.palette.primary.main
+        backgroundColor: theme.palette.primary.dark
     },
     floatingButtonAddTask: {
         position: 'fixed',
-        bottom: 10,
-        right: 90,
+        bottom: 80,
+        right: 0,
         color: 'white',
-        marginBottom: '12px',
-        marginRight: '12px',
-        backgroundColor: theme.palette.info.main
+        marginRight: '8px',
+        backgroundColor: theme.palette.primary.light
     },
     info: {
         margin: '5px',
         padding: '5px 5px 5px 10px',
-        fontSize: '0.8rem',
+        fontSize: '0.6rem',
         fontWeight: 'bold',
         backgroundColor: '#F5F5F5',
         width: '100%'
@@ -50,11 +42,51 @@ const useStyles = makeStyles((theme) => ({
         marginTop: '5px',
         padding: '10px',
 
+    },
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+    },
+    appbar: {
     }
 }));
 
-const Subject = (props) => {
 
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box p={2}>
+            {children}
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+  };
+
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+
+const Subject = (props) => {
+ 
     const classes = useStyles();
     const { materiaId } = useParams();
     const { setSubjectId, setSubjectName } = useContext(SubjectContext)
@@ -62,6 +94,8 @@ const Subject = (props) => {
     const [openInvite, setOpenInvite] = useState(false);
     const [openTask, setOpenTask] = useState(false);
     const [tasks, setTasks] = useState([]);
+
+    const [value, setValue] = React.useState(0);
 
     useEffect(() => {
         async function setSubjectData() {
@@ -78,6 +112,10 @@ const Subject = (props) => {
         cargarTareas();
         setSubjectData()
     }, [materiaId, setSubjectId, setSubjectName])
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
 
     const handleClickOpenInvite = () => {
         setOpenInvite(true);
@@ -114,29 +152,52 @@ const Subject = (props) => {
                 subjectId={materiaId}
                 acceptHandler={createTask}
             />}
-            <Grid container className={classes.container} spacing={3}>
+
+            
+            
+            
                 <Paper xs={12} sm={6} md={4} className={classes.info} variant="outlined" >
                     <p>Link al foro donde podés encontrar apuntes, examenes, trabajos practicos y más información de la materia <a href={link}  target="_blank">{link}</a></p>
-                </Paper>
-                {tasks && tasks.map((task) =>
+                </Paper>    
+                    <AppBar position="static" className={classes.appbar}>
+                        <Tabs value={value} onChange={handleChange} variant="fullWidth" aria-label="simple tabs example">
+                        <Tab label="Tareas pendientes" {...a11yProps(0)} />
+                        <Tab label="Tareas finalizadas" {...a11yProps(1)} />
+                        </Tabs>
+                    </AppBar>
+                    <TabPanel value={value} index={0}>
+                    <Grid container spacing={1}>
+                        {tasks && tasks.map((task) =>
+                            <Grid item xs={12} sm={6} md={4}  key={task.tareaId}>
+                                <CardTask data={task} history={props.history} acceptTaskHandler={createTask}/>
+                            </Grid>)
+                        }
+                     </Grid>
+                    </TabPanel>
+                    <TabPanel value={value} index={1}>
+                        Item Two
+                    </TabPanel>    
+                {/* tasks && tasks.map((task) =>
                     <Grid item xs={12} sm={6} md={4}  key={task.tareaId}>
                         <CardTask data={task} history={props.history} acceptTaskHandler={createTask}/>
                     </Grid>)
-                }
-            </Grid>
+                 */}
+
+            
+
 
             <IconButton
                 className={classes.floatingButtonInvite}
                 arial-label="Agregar colaborador"
                 onClick={handleClickOpenInvite}
             >
-                <PersonAddIcon style={{ fontSize: "40px" }} />
+                <PersonAddIcon style={{ fontSize: "28px" }} />
             </IconButton>
-            <Button variant="contained"
+            <IconButton variant="contained"
                     className={classes.floatingButtonAddTask}
                     onClick={handleClickOpenTask}>
-                    Agregar Tarea
-            </Button>
+                    <PostAddIcon style={{ fontSize: "28px" }} />
+            </IconButton>
         </>      
     );
 }
