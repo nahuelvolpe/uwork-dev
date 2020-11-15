ae9516f0ce7792c308d1cd5a4b10e754840ed11b
Daniel Suarez <dan.suarez@outlook.com>
2020-11-15 19:18:15 -0300

    dashboard fixed

--- src/uWork/components/Task/Task.js
+++ src/uWork/components/Task/Task.js
@@ -76,7 +76,7 @@ const DatePickerField = ({ field, form, ...other }) => {
       disabled={other.disabled}
       name={field.name}
       value={field.value}
-      format="MM/DD/yyyy"
+      format="DD/MM/yyyy"
       helperText={currentError}
       error={Boolean(currentError)}
       onError={error => {
