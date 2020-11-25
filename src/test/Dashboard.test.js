5ce2e8839144d13e1729de7a0919c81e2adfaef9
Matias Gimenez <meganiunmati@hotmail.com>
2020-11-24 23:23:44 -0300

    added button responsive tests in dashboard

--- src/test/Dashboard.test.js
+++ src/test/Dashboard.test.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import { render, cleanup, screen, fireEvent, wait, queryByAttribute } from '@testing-library/react'
+import { render, cleanup, fireEvent, wait, queryByAttribute } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
-import { mount, configure, shallow } from 'enzyme'
-import { act } from 'react-dom/test-utils';
+import { mount, configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
-import FormikField from '../uWork/components/FormikField/FormikField'
-import { Form, Formik } from 'formik'
-import { Button, Alert } from "@material-ui/core";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core";
 import Dashboard from '../uWork/components/Dashboard/Dashboard'
 import AuthenticationService from '../uWork/services/AuthenticationService'
 import * as MateriasService from '../uWork/services/MateriasService'
@@ -20,6 +17,20 @@ jest.mock('../uWork/services/UserService')
 configure({ adapter: new Adapter() })
 
 afterEach(cleanup)
+
+const SizeWrapper = (props) => {
+  const theme = createMuiTheme({
+    props: { MuiWithWidth: { initialWidth: props.size } },
+  })
+
+  return <MuiThemeProvider theme={theme}>{props.children}</MuiThemeProvider>;
+}
+
+export const customRender = (ui, options) => {
+  return render(ui, {
+    wrapper: (props) => <SizeWrapper {...props} {...options} />
+  })
+}
 
 const setUp = () => {
   const getById = queryByAttribute.bind(null, 'id')
@@ -45,17 +56,64 @@ describe('Dashboard', () => {
       })
     })
 
-    it('has a Alert and a Button', async () => {
-      //jest.setTimeout(20000)
-      const { container, getById } = setUp()
+    it('has an Alert', async () => {
+      const getById = queryByAttribute.bind(null, 'id')
+      const { container } = customRender(<Dashboard />, { size: 'lg' })
       AuthenticationService.getSessionUserId.mockReturnValue('id')
       MateriasService.getSubjects.mockResolvedValueOnce([])
-      //const { container } = render(/* <MemoryRouter> */<Dashboard />/* </MemoryRouter> */)
+      
       let alert
       await wait(() => {
         alert = getById(container, "guide-alert")
       })
+      expect(alert).not.toBe(null)
       expect(alert).toHaveTextContent("No tenés materias asignadas! Para agregar tu primer materia hacé click en el botón '+' de abajo a la derecha")
+      expect(AuthenticationService.getSessionUserId).toHaveBeenCalled()
+      expect(MateriasService.getSubjects).toHaveBeenCalled()
+    })
+    it('has a floating button on xs and sm width', async () => {
+      let floatingButton
+      let c
+      AuthenticationService.getSessionUserId.mockReturnValue('id')
+      MateriasService.getSubjects.mockResolvedValueOnce([])
+
+      c = customRender(<Dashboard />, { size: 'xs' })      
+      await wait(() => {
+        floatingButton = c.container.querySelector('button[title="Agregar Materia"]')
+      })
+      expect(floatingButton).not.toBe(null)
+
+      c = customRender(<Dashboard />, { size: 'sm' })      
+      await wait(() => {
+        floatingButton = c.container.querySelector('button[title="Agregar Materia"]')
+      })
+      expect(floatingButton).not.toBe(null)
+
+      expect(AuthenticationService.getSessionUserId).toHaveBeenCalled()
+      expect(MateriasService.getSubjects).toHaveBeenCalled()
+    })
+    it('has a button on md and lg width', async () => {
+      let button
+      let c
+      AuthenticationService.getSessionUserId.mockReturnValue('id')
+      MateriasService.getSubjects.mockResolvedValueOnce([])
+      c = customRender(<Dashboard />, { size: 'md' })
+
+      await wait(() => {
+        button = c.container.querySelector('button[label="Agregar Materia"]')
+      })
+      expect(button).not.toBe(null)
+      expect(button).toHaveTextContent("Agregar Materia")
+
+      c = customRender(<Dashboard />, { size: 'lg' })
+      await wait(() => {
+        button = c.container.querySelector('button[label="Agregar Materia"]')
+      })
+      expect(button).not.toBe(null)
+      expect(button).toHaveTextContent("Agregar Materia")
+
+      expect(AuthenticationService.getSessionUserId).toHaveBeenCalled()
+      expect(MateriasService.getSubjects).toHaveBeenCalled()
     })
   })
 }) 
